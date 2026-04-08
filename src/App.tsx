8c3f1447abaefb9facc34645df1fbd196e2ab669
ByteZero Digital <contact@bytezeroinc.com>
2026-04-08 18:35:32 -0400

    Make VexPage the default route at /

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import OriginalPage from './pages/OriginalPage'
-import CinematicPage from './pages/CinematicPage'
 import VexPage from './pages/VexPage'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<OriginalPage />} />
-        <Route path="/cinematic" element={<CinematicPage />} />
+        <Route path="/" element={<VexPage />} />
         <Route path="/vex" element={<VexPage />} />
       </Routes>
     </BrowserRouter>
